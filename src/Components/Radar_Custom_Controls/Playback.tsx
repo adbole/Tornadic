712518d854d5221cae27ffcec7da71645b273dc1
adbole <109166687+adbole@users.noreply.github.com>
2022-08-20 05:56:09 -0500

    The PlayPauseButton and Playback components in Playback.tsx are now all classes due to how both use states.

--- src/Components/Radar_Custom_Controls/Playback.tsx
+++ src/Components/Radar_Custom_Controls/Playback.tsx
@@ -4,7 +4,7 @@ import L from 'leaflet';
 import { useMap } from 'react-leaflet';
 import { createControlComponent } from '@react-leaflet/core';
 
-import {Play, Pause} from '../../svgs/svgs'
+import { Play, Pause } from '../../svgs/svgs'
 
 //Uses floor function to keep remainder the same sign as divisor. 
 const mod = (x: number, div: number) => {
@@ -43,183 +43,197 @@ type AvailableLayer = {
 }
 
 type LayerDict = Record<LayerTypes, AvailableLayer>
+type PlaybackProps = { MAP: L.Map }
 
-const Playback = () => {
-    const MAP = useMap();
-    let host: string;
-    const availableLayers = {Satellite: {}, Radar: {}} as LayerDict;
-    
-    let activeLayer: LayerTypes;
-    let animationTimer: NodeJS.Timeout | null;
-
-    MAP.on('baselayerchange', function(e) {
-        availableLayers[activeLayer].slider?.current?.classList.toggle('hide');
-        activeLayer = e.name as LayerTypes
-        availableLayers[activeLayer].slider?.current?.classList.toggle('hide');
-
-        ShowFrame(availableLayers[activeLayer].currentAnimPos)
-    })
-
-    //Get radar data
-    const xhtml = new XMLHttpRequest()
-    xhtml.open("GET", "https://api.rainviewer.com/public/weather-maps.json", false);
-    xhtml.onload = () => {
-        if(xhtml.status !== 200) {
-            console.error("Couldn't receive radar data")
-            return;
-        }
-
-        const response: ApiResponse = JSON.parse(xhtml.response)
-        
-        host = response.host
-
-        //Prepare frames
-        availableLayers.Radar.frames = response.radar.past.concat(response.radar.nowcast);
-        availableLayers.Satellite.frames = response.satellite.infrared;
-
-        const layersControl = L.control.layers().addTo(MAP);
-        
-        //Prepare all other information and intialize needed objects
-        for(const key in availableLayers) {
-            const layerKey = key as LayerTypes;
-            availableLayers[layerKey].layerGroup = L.layerGroup().addTo(MAP);
-            availableLayers[layerKey].loadedLayers = [];
-            availableLayers[layerKey].currentAnimPos = 0;
-
-            layersControl.addBaseLayer(availableLayers[layerKey].layerGroup, layerKey);
-        }
-
-        //Since the default is radar the lastFramePos will be determined by the last past frame
-        activeLayer = LayerTypes.Radar;
-        const lastFramePos = response.radar.past.length - 1; 
-
-        ShowFrame(lastFramePos)
+class PlayPauseButton extends React.PureComponent<{Play: VoidFunction, Stop: Function}, {isPlaying: boolean}> {
+    state = {
+        isPlaying: false
     }
-    xhtml.send();
 
-    function ShowFrame(loadPos: number) {
+    OnCLick(e: React.MouseEvent) {
+        e.stopPropagation();
+
+        if(!this.props.Stop()) {
+            this.setState({isPlaying: true})
+            this.props.Play();
+        }
+        else this.setState({isPlaying: false})
+    }
+
+    render() {
+        return (
+            <div onClick={this.OnCLick.bind(this)}>
+                { this.state.isPlaying ? <Pause /> : <Play /> }
+            </div>
+        )
+    }
+}
+
+class Playback extends React.Component<PlaybackProps, { active: LayerTypes}> {
+    host!: string;
+    availableLayers = {Satellite: {}, Radar: {}} as LayerDict;
+    animationTimer!: NodeJS.Timeout | null;
+    timeLine!: React.RefObject<HTMLInputElement>
+
+    constructor(props: PlaybackProps) {
+        super(props);
+
+        this.state = {
+            active: LayerTypes.Radar
+        }
+        this.timeLine = React.createRef<HTMLInputElement>();
+
+        this.Setup();
+
+        this.props.MAP.on('baselayerchange', (e) => this.UpdateLayer(e.name as LayerTypes));
+
+        //Due to how layers are added, the baselayerchange event will not fire until the layers have been changed at least twice. 
+        //To work aroundt this, a click is simulated on the controls at load to force the baselayerchange event to fire as expected.
+        [...document.querySelectorAll('.leaflet-control-layers-selector')].forEach((el) => {
+            el.dispatchEvent(new Event('click'))
+        })
+    }
+
+    Setup() {
+        const xhtml = new XMLHttpRequest()
+        xhtml.open("GET", "https://api.rainviewer.com/public/weather-maps.json", false);
+        xhtml.onload = () => {
+            if(xhtml.status !== 200) {
+                console.error("Couldn't receive radar data")
+                return;
+            }
+
+            const response: ApiResponse = JSON.parse(xhtml.response)
+            
+            this.host = response.host
+
+            //Prepare frames
+            this.availableLayers.Radar.frames = response.radar.past.concat(response.radar.nowcast);
+            this.availableLayers.Satellite.frames = response.satellite.infrared;
+
+            const layersControl = L.control.layers().addTo(this.props.MAP);
+            
+            //Prepare all other information and intialize needed objects
+            for(const key in this.availableLayers) {
+                const layerKey = key as LayerTypes;
+                this.availableLayers[layerKey].layerGroup = L.layerGroup().addTo(this.props.MAP);
+                this.availableLayers[layerKey].loadedLayers = [];
+                this.availableLayers[layerKey].currentAnimPos = 0;
+
+                layersControl.addBaseLayer(this.availableLayers[layerKey].layerGroup, layerKey);
+            }
+
+            //Since the default is radar the lastFramePos will be determined by the last past frame
+            const lastFramePos = response.radar.past.length - 1; 
+
+            this.ShowFrame(lastFramePos)
+        }
+        xhtml.send();
+    }
+
+    UpdateLayer(layer: LayerTypes) {
+        this.setState({active: layer})
+        this.ShowFrame(this.availableLayers[layer].currentAnimPos)
+    }
+
+    ShowFrame(loadPos: number) {
         //Determine how to load the frame after this one
-        const preLoadDirection = loadPos - availableLayers[activeLayer].currentAnimPos > 0 ? 1 : -1
+        const preLoadDirection = loadPos - this.availableLayers[this.state.active].currentAnimPos > 0 ? 1 : -1
 
-        ChangeRadarPos(loadPos, false); //Load this frame
-        ChangeRadarPos(loadPos + preLoadDirection, true); //Preload the next frame
+        this.ChangeRadarPos(loadPos, false); //Load this frame
+        this.ChangeRadarPos(loadPos + preLoadDirection, true); //Preload the next frame
     }
 
-    function ChangeRadarPos(position: number, preloadOnly: boolean) {
-        const activeFrames = availableLayers[activeLayer].frames;
-        const activeLayers = availableLayers[activeLayer].loadedLayers;
+    ChangeRadarPos(position: number, preloadOnly: boolean) {
+        const activeFrames = this.availableLayers[this.state.active].frames;
+        const activeLayers = this.availableLayers[this.state.active].loadedLayers;
 
         if(position < 0 || position > activeFrames.length - 1) {
             position = mod(position, activeFrames.length)
         }
 
-        const currentFrame = activeFrames[availableLayers[activeLayer].currentAnimPos];
+        const currentFrame = activeFrames[this.availableLayers[this.state.active].currentAnimPos];
         const nextFrame = activeFrames[position];
 
-        AddLayer(nextFrame);
+        this.AddLayer(nextFrame);
 
         if(preloadOnly) return;
 
-        availableLayers[activeLayer].currentAnimPos = position;
+        this.availableLayers[this.state.active].currentAnimPos = position;
 
         if(activeLayers[currentFrame.time]) {
             activeLayers[currentFrame.time].setOpacity(0)
         }
 
-        if(availableLayers[activeLayer].slider?.current !== undefined) {
-            availableLayers[activeLayer].slider.current!.value = nextFrame.time.toString();
+        if(this.availableLayers[this.state.active].slider?.current !== undefined) {
+            this.availableLayers[this.state.active].slider.current!.value = nextFrame.time.toString();
+        }
+
+        if(this.timeLine.current !== null) {
+            this.timeLine.current.value = nextFrame.time.toString()
         }
         activeLayers[nextFrame.time].setOpacity(1);
     }
 
-    function AddLayer(frame: Tile) {
-        const activeLayers = availableLayers[activeLayer].loadedLayers;
+    AddLayer(frame: Tile) {
+        const activeLayers = this.availableLayers[this.state.active].loadedLayers;
 
         //If this frame hasn't been added as a layer yet do so now
         if(!activeLayers[frame.time]) {
-            const color = activeLayer === LayerTypes.Radar ? 6 : 0
-            activeLayers[frame.time] = new L.TileLayer(host + frame.path + "/512/{z}/{x}/{y}/" + color + "/1_0.png", {
+            const color = this.state.active === LayerTypes.Radar ? 6 : 0
+            activeLayers[frame.time] = new L.TileLayer(this.host + frame.path + "/512/{z}/{x}/{y}/" + color + "/1_0.png", {
                 opacity: 0.0,
                 zIndex: frame.time
             });
         }
 
         //If the layer of the frame hasn't been added yet do so now
-        if(!MAP.hasLayer(activeLayers[frame.time])) {
-            availableLayers[activeLayer].layerGroup.addLayer(activeLayers[frame.time])
+        if(!this.props.MAP.hasLayer(activeLayers[frame.time])) {
+            this.availableLayers[this.state.active].layerGroup.addLayer(activeLayers[frame.time])
         }
     }
 
     //Play will show the next frame every 0.5s.
-    function PlayAnim() {
-        ShowFrame(availableLayers[activeLayer].currentAnimPos + 1);
+    PlayAnim() {
+        this.ShowFrame(this.availableLayers[this.state.active].currentAnimPos + 1);
 
-        animationTimer = setTimeout(PlayAnim, 500);
+        this.animationTimer = setTimeout(this.PlayAnim.bind(this), 500);
     }
 
-    function Stop() {
-        if(animationTimer) {
-            clearTimeout(animationTimer);
-            animationTimer = null
+    Stop() {
+        if(this.animationTimer) {
+            clearTimeout(this.animationTimer);
+            this.animationTimer = null
             return true;
         }
 
         return false;
     }
 
-
-
-    //Due to how layers are added, the baselayerchange event will not fire until the layers have been changed at least twice. 
-    //To work aroundt this, a click is simulated on the controls at load to force the baselayerchange event to fire as expected.
-    [...document.querySelectorAll('.leaflet-control-layers-selector')].forEach((el) => {
-        el.dispatchEvent(new Event('click'))
-    })
-
-    const PlayPauseComponent = () => {
-        const [isPlaying, setIsPlaying] = React.useState(false);
-
-        function PlayStop(e: React.MouseEvent) {
-            e.stopPropagation()
-
-            if(!Stop()) {
-                setIsPlaying(true)
-                PlayAnim();
-            }
-            else setIsPlaying(false)
-        }
-
+    render() {
         return (
-            <div onClick={PlayStop}>
-                { isPlaying ? <Pause /> : <Play /> }
-            </div>
-        )
-    }
-    const PlaybackComponent = () => (
         <>
-            <PlayPauseComponent />
+            <PlayPauseButton Play={this.PlayAnim.bind(this)} Stop={this.Stop.bind(this)}/>
             <div>
-                {
-                    Object.entries(availableLayers).map((pair) => {
-                        const className = activeLayer === pair[0] ? "" : "hide";
-                        availableLayers[pair[0] as LayerTypes].slider = React.createRef<HTMLInputElement>();
-
-                        return (
-                            <React.Fragment key={pair[0]}>
-                                <input key={pair[0]} ref={pair[1].slider} type="range" className={className} list={"radar-" + pair[0]} min={pair[1].frames[0].time} max={pair[1].frames[pair[1].frames.length - 1].time} step={600} defaultValue={pair[1].frames[pair[1].currentAnimPos].time} />
-                                <datalist id={"radar-" + pair[0]}>
-                                    {
-                                        pair[1].frames.map(frame => (
-                                            <option key={frame.time} value={frame.time}></option>
-                                        ))
-                                    }
-                                </datalist>
-                            </React.Fragment>
-                        )
-                    })
-                }
+                <input type="range" list="radar-list" ref={this.timeLine} min={this.availableLayers[this.state.active].frames[0].time} 
+                    max={this.availableLayers[this.state.active].frames[this.availableLayers[this.state.active].frames.length - 1].time} 
+                    step={600}
+                    defaultValue={this.availableLayers[this.state.active].frames[this.availableLayers[this.state.active].currentAnimPos].time} 
+                />
+                <datalist id="radar-list">
+                    {
+                        this.availableLayers[this.state.active].frames.map(frame => (
+                            <option key={frame.time} value={frame.time}></option>
+                        ))
+                    }
+                </datalist>
             </div>
         </>
-    )
+    )}
+}
+
+const PlaybackLeafletWrapper = () => {
+    const MAP = useMap();
 
     const Home = L.Control.extend({
         options: {
@@ -229,12 +243,12 @@ const Playback = () => {
             const div = L.DomUtil.create("div", "leaflet-custom-control");
             div.id = "playback"
             L.DomEvent.disableClickPropagation(div)
-            ReactDOM.createRoot(div).render(<PlaybackComponent />)
+            ReactDOM.createRoot(div).render(<Playback MAP={MAP}/>)
             return div;
         }
     });
 
     return new Home();
-};
+}
 
-export default createControlComponent(Playback)
+export default createControlComponent(PlaybackLeafletWrapper)
