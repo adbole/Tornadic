db4f9609960cf1f43f2dfa3ab1cc3bff54d7fd18
adbole <109166687+adbole@users.noreply.github.com>
2023-05-17 17:03:13 -0500

    Added additional logic to Radar.Opacity to provide more compatibility with touch devices.

--- src/Components/Radar/Radar.Opacity.tsx
+++ src/Components/Radar/Radar.Opacity.tsx
@@ -4,8 +4,16 @@ import { CircleSlashes } from '../../svgs/radar';
 const Opacity = ({value, setOpacity}: {value: number, setOpacity: (x: number) => void}) => {
     const [hover, setHover] = React.useState(false);
 
+    //Fallback for touch devices
+    React.useEffect(() => {
+        const reset = () => setHover(false);
+
+        document.body.addEventListener("click", reset);
+        return () => document.body.removeEventListener("click", reset);
+    }, []);
+
     return (
-        <div className="leaflet-custom-control leaflet-control" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+        <div className="leaflet-custom-control leaflet-control" onClick={(e: React.MouseEvent<HTMLDivElement>) => e.stopPropagation()} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
             {!hover && <div className='leaflet-control-toggle'><CircleSlashes/></div>}
             {hover && 
                 <div id="opacity">
