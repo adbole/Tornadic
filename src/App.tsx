501c973ddd6e27ac1a4753c5178d47fd0038c72d
adbole <109166687+adbole@users.noreply.github.com>
2023-06-04 22:08:13 -0500

    Initial location permission check has been moved to index from App.

--- src/App.tsx
+++ src/App.tsx
@@ -7,68 +7,29 @@ import Wind from 'Components/Wind';
 import SunTime from 'Components/SunTime';
 import Alert from 'Components/Alert';
 import Pressure from 'Components/Pressure';
+import { AirUV, DayValues } from 'Components/SimpleComponents';
 
 //Contexts
 import ModalContext from 'Components/Contexts/ModalContext';
 import WeatherContext from 'Components/Contexts/Weather';
-import { AirUV, DayValues } from 'Components/SimpleComponents';
 
-import React from 'react';
-import MessageScreen from 'Components/MessageScreen';
-import { ExclamationTriangle } from 'svgs';
-import { Cursor } from 'svgs/radar';
-
-const App = () => {
-    //Before tornadic can load, it needs location permissions. 
-    //Check if it has been authorized
-    const [permissionStatus, setPermissionStatus] = React.useState<PermissionState | null>(null);
-
-    React.useEffect(() => {
-        navigator.permissions.query({name: "geolocation"}).then(status => setPermissionStatus(status.state));
-    }, []);
-
-    if(permissionStatus === null) {
-        return null;
-    }
-    else if(permissionStatus === "prompt") {
-        return (
-            <MessageScreen>
-                <Cursor/>
-                <p>Tornadic requires location permissions to provide forcasts for your area.</p>
-                <div>
-                    <button type="button" onClick={() => navigator.geolocation.getCurrentPosition(() => setPermissionStatus("granted"), () => setPermissionStatus("denied"))}>Provide Location</button>
-                </div>
-            </MessageScreen>
-        );
-    }
-    else if(permissionStatus === "denied") {
-        return (
-            <MessageScreen>
-                <ExclamationTriangle/>
-                <p>Tornadic does not have permission to access your location</p>
-            </MessageScreen>
-        );
-    }
-    else {
-        return (
-            <>
-                <WeatherContext>
-                    <ModalContext>
-                            <Now/>
-                            <Alert/>
-                            <Hourly />
-                            <DayValues/>
-                            <AirUV />
-                            <Daily />
-                            <Radar />
-                            <Wind/>
-                            <Pressure/>
-                            <SunTime/>
-                    </ModalContext>
-                </WeatherContext>
-            </>
-        );
-    }
-};
+const App = () => (
+    <>
+        <WeatherContext>
+            <ModalContext>
+                    <Now/>
+                    <Alert/>
+                    <Hourly />
+                    <DayValues/>
+                    <AirUV />
+                    <Daily />
+                    <Radar />
+                    <Wind/>
+                    <Pressure/>
+                    <SunTime/>
+            </ModalContext>
+        </WeatherContext>
+    </>
+);
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,24 @@
 //React
 import ReactDOM from 'react-dom/client';
 import React from 'react';
-// import reportWebVitals from './reportWebVitals';
 
 //Components
 import App from 'App';
 import MessageScreen from 'Components/MessageScreen';
 import { WifiOff } from 'svgs';
+import { ExclamationTriangle } from 'svgs';
+import { Cursor } from 'svgs/radar';
 
-const OnlineOfflineManager = () => {
+const Index = () => {
     const [online, setOnline] = React.useState(navigator.onLine);
 
+    //Before tornadic can load, it needs location permissions. 
+    //Check if it has been authorized
+    const [permissionStatus, setPermissionStatus] = React.useState<PermissionState | null>(null);
+
     React.useEffect(() => {
+        navigator.permissions.query({name: "geolocation"}).then(status => setPermissionStatus(status.state));
+
         const handleOnline = () => setOnline(true);
         const handleOffline = () => setOnline(false);
 
@@ -24,33 +31,44 @@ const OnlineOfflineManager = () => {
         };
     }, []);
 
-    return (
-        <>
-            {
-                online 
-                ? <App/> 
-                : (
-                    <MessageScreen>
-                        <WifiOff/>
-                        <p>Tornadic requires an internet connection to function properly</p>
-                    </MessageScreen>
-                )
-            }
-        </>
-    );
+    if(online) {
+        if(permissionStatus === null) {
+            return null;
+        }
+        else if(permissionStatus === "prompt") {
+            return (
+                <MessageScreen>
+                    <Cursor/>
+                    <p>Tornadic requires location permissions to provide forcasts for your area.</p>
+                    <div>
+                        <button type="button" onClick={() => navigator.geolocation.getCurrentPosition(() => setPermissionStatus("granted"), () => setPermissionStatus("denied"))}>Provide Location</button>
+                    </div>
+                </MessageScreen>
+            );
+        }
+        else if(permissionStatus === "denied") {
+            return (
+                <MessageScreen>
+                    <ExclamationTriangle/>
+                    <p>Tornadic does not have permission to access your location</p>
+                </MessageScreen>
+            );
+        }
+        else {
+            return <App/>;
+        }
+    }
+    else {
+        return (
+            <>
+                <MessageScreen>
+                    <WifiOff/>
+                    <p>Tornadic requires an internet connection to function properly</p>
+                </MessageScreen>
+            </>
+        );
+    }
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(<OnlineOfflineManager />);
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+root.render(<Index />);
