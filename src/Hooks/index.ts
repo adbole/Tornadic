eebdb8a0a2fa8be967696d2ec2347556d481ed3a
adbole <109166687+adbole@users.noreply.github.com>
2023-07-20 20:23:25 -0500

    Added new useOnlineOffline hook

--- src/Hooks/index.ts
+++ src/Hooks/index.ts
@@ -3,3 +3,4 @@ export { default as useBooleanState } from "./useBooleanState";
 export { default as useDragScroll } from "./useDragScroll";
 export { default as useLocalStorage } from "./useLocalStorage";
 export { default as useNullableState } from "./useNullableState";
+export { default as useOnlineOffline } from "./useOnlineOffline";
---
+++ src/Hooks/useOnlineOffline.ts
@@ -0,0 +1,21 @@
+import React from "react";
+
+import useBooleanState from "./useBooleanState";
+
+
+export default function useOnlineOffline() {
+    const [online, setOnlineTrue, setOnlineFalse] = useBooleanState(navigator.onLine);
+
+
+    React.useEffect(() => {
+        window.addEventListener("online", setOnlineTrue);
+        window.addEventListener("offline", setOnlineFalse);
+
+        return () => {
+            window.removeEventListener("online", setOnlineTrue);
+            window.removeEventListener("offline", setOnlineFalse);
+        };
+    }, [setOnlineFalse, setOnlineTrue]);
+
+    return online;
+}
