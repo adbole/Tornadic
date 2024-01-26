0475155c3a4fac8936059eacf9cdffb16a349cf7
adbole <109166687+adbole@users.noreply.github.com>
2024-01-25 22:45:42 -0600

    Remove unused circle-slashes.svg

--- src/svgs/radar/index.ts
+++ src/svgs/radar/index.ts
@@ -1,6 +1,5 @@
 /// <reference types="vite-plugin-svgr/client" />
 
-export { default as CircleSlashes } from "./circle-slashes.svg?react";
 export { default as LocationDot } from "./current-location-dot.svg?react";
 export { default as Cursor } from "./cursor-fill.svg?react";
 export { default as Grid } from "./grid.svg?react";
